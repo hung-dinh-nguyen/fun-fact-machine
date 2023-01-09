081904b4b542ad1fa4b31b565ac17d5966540c40
Hung Dinh Nguyen <hung.dinh.nguyen.b@gmail.com>
2023-01-09 17:47:22 -0500

    Worked on stock-tracker, but ran into problems with node.js version
    Current node.js version does not have Fetch API built-in. May need to update node.js or install add-on

--- src/components/stock-tracker/stock-tracker.js
+++ src/components/stock-tracker/stock-tracker.js
@@ -1,2 +1,12 @@
 import 'stock-tracker.css'
 
+const StockTracker = ({}) => {
+
+    fetch('https://api.polygon.io/v1/open-close/SPY/2023-01-06?adjusted=true&apiKey=GTg5Bir0PyXXJNO2EXN5syHMW9blMFdv')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+
+
+export default StockTracker; 
