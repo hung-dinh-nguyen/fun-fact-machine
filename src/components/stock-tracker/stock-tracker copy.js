a5a681607475b4e36e14c8e36806564c2b43b3a4
Hung Dinh Nguyen <hung.dinh.nguyen.b@gmail.com>
2023-01-12 23:13:25 -0500

    Needed to update node.js

--- src/components/stock-tracker/stock-tracker copy.js
+++ src/components/stock-tracker/stock-tracker copy.js
@@ -1,3 +1,4 @@
 fetch('https://api.polygon.io/v1/open-close/SPY/2023-01-06?adjusted=true&apiKey=GTg5Bir0PyXXJNO2EXN5syHMW9blMFdv')
   .then((response) => response.json())
   .then((data) => console.log(data));
+  
