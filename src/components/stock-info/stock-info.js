baf46ee1ddc7148477af3b68c8ee83dd1f30df35
Hung Dinh Nguyen <hung.dinh.nguyen.b@gmail.com>
2023-01-26 19:26:13 -0500

    Fixed the nomenclature in stock-info.js to display the header and paragraph tags appropriately

--- src/components/stock-info/stock-info.js
+++ src/components/stock-info/stock-info.js
@@ -4,25 +4,23 @@ const StockInfo = (props) => {
     return (
 
         <div className='stockinfo-div'>
-            <textarea
+            <h2
                 id='stockinfo-text'
                 className='stockinfo-text'
                 type='text'
-                value={props.stockTicker}
-            />
 
-            <textarea
+            >{props.stockTicker}</h2>
+
+            <p
                 id='opening-value'
                 className='opening-value'
                 type='text'
-                value={props.stockData.open}
-            />
-            <textarea
+            >{props.stockData.open}</p>
+            <p
                 id='closing-value'
                 className='closing-value'
                 type='text'
-                value={props.stockData.close}
-            />
+            >{props.stockData.close}</p>
     
         </div>
     )
